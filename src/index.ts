d5b750abc1429a22edf264aae754bf1d613ad6a9
kenneth <kennytakudzwa@gmail.com>
2024-08-08 03:21:56 +0200

    initial commit of the backend

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import myUserRoute from './routes/MyUserRoute'
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log('Connected to database!'))
+
+const app = express()
+app.use(express.json()) //middleware to automatically convert the body of any request made to our API into JSON
+app.use(cors())
+
+app.get('/health', async (req: Request, res: Response) => {
+  res.send({ message: 'health OK!' })
+})
+
+// /api/my/user
+app.use('/api/my/user', myUserRoute)
+
+app.listen(7000, () => {
+  console.log('server started on localhost:7000')
+})
