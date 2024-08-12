8cfbbe2ecc48a7e9fb2bf0f2b40d71b161529d88
kenneth <kennytakudzwa@gmail.com>
2024-08-12 02:47:12 +0200

    the restaurant api

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import mongoose from 'mongoose'
 import myUserRoute from './routes/MyUserRoute'
 import { v2 as cloudinary } from 'cloudinary'
+import myRestaurantRoute from './routes/MyRestaurantRoute'
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -23,8 +24,8 @@ app.get('/health', async (req: Request, res: Response) => {
   res.send({ message: 'health OK!' })
 })
 
-// /api/my/user
 app.use('/api/my/user', myUserRoute)
+app.use('/api/my/restaurant', myRestaurantRoute)
 
 app.listen(7000, () => {
   console.log('server started on localhost:7000')
